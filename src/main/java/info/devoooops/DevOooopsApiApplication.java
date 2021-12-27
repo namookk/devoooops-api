9b84e31d61eeaf17991e00668f6eea02a2cdcc20
namookk <papakang22@naver.com>
2021-12-27 17:38:32 +0900

    add : init Database Configuration

--- src/main/java/info/devoooops/DevOooopsApiApplication.java
+++ src/main/java/info/devoooops/DevOooopsApiApplication.java
@@ -3,10 +3,9 @@ package info.devoooops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class, DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class})
 public class DevOooopsApiApplication {
 
     public static void main(String[] args) {
