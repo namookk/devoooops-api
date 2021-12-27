5d2df29607450673b7fe8bbaae3efd4afe25ab33
namookk <papakang22@naver.com>
2021-12-27 12:59:04 +0900

    init : create project

---
+++ src/main/java/info/devoooops/DevOooopsApiApplication.java
@@ -0,0 +1,13 @@
+package info.devoooops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOooopsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOooopsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/devoooops/DevOooopsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package info.devoooops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOooopsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
